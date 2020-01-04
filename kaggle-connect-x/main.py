e46728a67d89373a709a3b24e5084fdce2b11e8f
Jovan Sardinha <jovan.sardinha@gmail.com>
2020-01-04 04:08:54 -0800

    commented steps on main.py

--- kaggle-connect-x/main.py
+++ kaggle-connect-x/main.py
@@ -4,12 +4,16 @@ import utils
 from datetime import datetime
 
 if __name__ == "__main__":
+    # initialize
     env = make("connectx", debug=True)
-
     env.reset()
+
+    #get agent
     my_agent = agents.random_agent.agent
     env.run([my_agent, "random"])
-    print(env.render(mode="ansi", width=500, height=450))
+
+    # # sample env.render
+    # print(env.render(mode="ansi", width=500, height=450))
 
     # Play as first position against random agent.
     trainer = env.train([None, "random"])
@@ -27,4 +31,4 @@ if __name__ == "__main__":
     utils.compare_against_baseline(my_agent)
 
     time_stamp = datetime.utcnow().strftime("%y_%m_%d__%f")
-    utils.write_agent_to_file(my_agent, f"submission_{time_stamp}.py")
+    # utils.write_agent_to_file(my_agent, f"submission_{time_stamp}.py")
